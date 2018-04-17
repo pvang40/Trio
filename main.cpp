3fec330f51d4223b2441bf651406736f8e487e4d
pvang40 <pvang40@mail.csuchico.edu>
2018-04-17 16:02:29 -0700

    lab 21

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include<string> 
+
 using namespace std;
+
+int printSort(int&, int&, int&);
+int initialRed; 
+int initialGreen;
+int initialBlue;
+#include <cstdlib>
+#include<ctime>
 
 int main()
 {
@@ -9,8 +18,11 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
+initialRed= red;
+initialBlue= blue;
+initialGreen=green;
 
-
+printSort(red, green, blue); 
 
 
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -19,3 +31,33 @@ int main()
   return 0;
   //...END OF "DO NOT CHANGE" AREA
 }
+
+int printSort(int& first, int& middle, int& last)
+
+{ 
+if (initialRed > initialGreen) 
+{
+ if (initialRed > initialBlue) last = initialRed; 
+ else if (initialRed < initialBlue) 
+ middle = initialRed; } 
+ if (initialGreen > initialRed) 
+ { if (initialGreen > initialBlue) 
+ first = initialGreen; 
+ else if (initialGreen <initialBlue) 
+ middle = initialGreen; } 
+ if (initialBlue > initialGreen) 
+ { if (initialBlue > initialRed) 
+ first = initialBlue; 
+ else if (initialBlue < initialRed) middle = initialBlue; }
+  if (initialGreen != first && initialGreen != middle) 
+  middle = initialGreen; 
+  if (initialRed != first && initialRed != middle) 
+  last = initialRed; 
+  if (initialBlue != first && initialBlue != middle) 
+  last = initialBlue;
+
+return first; 
+return middle; 
+return last; 
+  } 
+
